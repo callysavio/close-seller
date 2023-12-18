adf318f36ccd423ab9ed29f76c6958deb7d35339
PLD908 <oluyededivine@gmail.com>
2023-12-18 22:21:30 +0100

    Added some code

---
+++ buy-ticket.js
@@ -0,0 +1,9 @@
+function offCanvas() {
+    let x = document.getElementById("showRight");
+    x.style.display = "block";
+};
+
+function cancel() {
+    let x = document.getElementById("showRight");
+    x.style.display = "none";
+};
